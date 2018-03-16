ea65b4f8460a69fd99d447dbc0803d73801efaba
mydearvengeance <tanicandy97@hotmail.com>
2018-03-15 23:15:09 -0600

    Commit comentario2 :v

--- app/src/main/java/com/example/tania/findme/MainActivity.java
+++ app/src/main/java/com/example/tania/findme/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Prueba 1 :v
+        //Prueba 2 :v
     }
 }
