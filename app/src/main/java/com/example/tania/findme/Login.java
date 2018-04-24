9b6df8864500ae2d02ad18f5eee6449a0e38acf7
David Camargo <davidazuara1525@gmail.com>
2018-04-24 11:30:08 -0500

    Validaciones
    gugugo

--- app/src/main/java/com/example/tania/findme/Login.java
+++ app/src/main/java/com/example/tania/findme/Login.java
@@ -13,9 +13,7 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
+        
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
